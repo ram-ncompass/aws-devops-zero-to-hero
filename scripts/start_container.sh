45921a623b111ff887c6ed6f3c85f5acbb715d71
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-26 22:43:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 sudo docker pull ramkumar58059/simple-python-flask-app
-echo " here152"
+echo "here152"
 
 # Run the Docker image as a container
 sudo docker run -d -p 5000:5000 ramkumar58059/simple-python-flask-app
