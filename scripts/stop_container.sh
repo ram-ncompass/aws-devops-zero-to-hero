89d05f2dacb97ce4f381423005383086cdb61747
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-26 22:21:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-sudo containerid=`docker ps | awk -F " " '{print $1}'`
-sudo docker rm -f $containerid
+# Find the ID of the running container
+container_id=$(sudo docker ps -q)
+
+# Check if a container is running
+if [ -n "$container_id" ]; then
+  # Stop and remove the running container
+  sudo docker stop "$container_id"
+  sudo docker rm "$container_id"
+else
+  echo "No running container found."
+fi
+
