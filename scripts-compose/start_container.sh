91e76426f7e6d4815ab94d6948f9a0c1dc3922ba
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-27 15:02:20 +0530

    debug

--- scripts-compose/start_container.sh
+++ scripts-compose/start_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Change to the directory where docker-compose.yml is located
-
+pwd
 # Start the containers defined in docker-compose.yml in the background
 sudo docker-compose up -d
--- scripts-compose/stop_container.sh
+++ scripts-compose/stop_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Change to the directory where docker-compose.yml is located
 # cd /path/on/ec2-instance
-
+pwd
 # Stop and remove the containers defined in docker-compose.yml
 sudo docker-compose down
