422422e14ae2dd37957f3e2e1dad0f2903258d5f
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-27 14:54:50 +0530

    Move docker-compose.yml

--- scripts-compose/start_container.sh
+++ scripts-compose/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Change to the directory where docker-compose.yml is located
-# cd /path/on/ec2-instance
 
 # Start the containers defined in docker-compose.yml in the background
 sudo docker-compose up -d
