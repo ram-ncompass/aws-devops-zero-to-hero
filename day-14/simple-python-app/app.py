996d20d24575bedb2bed31cede86379147f4e1a1
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-27 16:05:02 +0530

    submit

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! Added content !!! Added content4'
+    return 'Hello world! Added content !!! Added content444'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
 
--- scripts-compose/stop_container.sh
+++ scripts-compose/stop_container.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Change to the directory where docker-compose.yml is located
-# cd /path/on/ec2-instance
 pwd
-ls
 cd /home/ubuntu/docker-files
 # Stop and remove the containers defined in docker-compose.yml
 sudo docker-compose down
