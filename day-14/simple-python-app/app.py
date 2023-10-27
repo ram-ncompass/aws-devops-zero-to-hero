2f923cbd3adc504dea17d82cb97deb02476ba438
ram-ncompass <122342893+ram-ncompass@users.noreply.github.com>
2023-10-27 16:29:37 +0530

    submit

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! Added content !!! Added content444'
+    return 'Hello world! Added content !!! Added new content'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000)
